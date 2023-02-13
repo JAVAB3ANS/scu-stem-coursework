a1deeaa0f0d9775a81802ca40bc7d34b7703c8f3
Jason Vu <jvu@scu.edu>
2023-02-12 23:27:17 -0800

    update lab 4

--- coen-146/lab-4/webserver.py
+++ coen-146/lab-4/webserver.py
@@ -33,8 +33,15 @@ if __name__ == "__main__":
 
         # gets the request from the client
         message = connectionSocket.recv(1024)
-        print(message)
   
+        # TypeError: argument should be integer or bytes-like object, not 'str'
+        message = message.decode("utf-8")
+        print(message)
+
+        # STUDENT WORK
+
+        # get file name from the request
+
         start_f = message.find("/")
         end_f = message.find(" HTTP/1.1")
 
@@ -59,25 +66,20 @@ if __name__ == "__main__":
                 response_headers = { "Content-Type": "image/jpeg; encoding=utf8" }
             else:
                 print("Invalid file type, we only support html and jpg!")
-                continue
-            
-            connectionSocket.send("HTTP/1.1 200 OK\n" + "Content:" + response_headers["Content-Type"] + "\n\r\n")
-  
-            # STUDENT WORK  
+                continue 
 
-            f = open(file_name, "r") 
+            # fix TypeError: a bytes-like object is required, not 'str'
+ 
+            connectionSocket.send("HTTP/1.1 200 OK\n".encode("utf-8"))
+  
+            f = open(file_name, "r")
             content = f.read()
             connectionSocket.send(content)
-
-            #f_lines = list(f)
-
-            #for line in f_lines:
-            #   connectionSocket.send(line) 
                  
         # Raised when an I/O operation (such as a print statement, the built-in open() function or a method of a file object) fails for an I/O-related reason, e.g., "file not found" or "disk full"
         except IOError:
             # STUDENT WORK 
-            connectionSocket.send("HTTP/1.1 404 NOT FOUND\n" + "Content:" + "text/html; encoding=utf8" + "\n\r\n")
+            connectionSocket.send("HTTP/1.1 404 Not Found\n" + "Content-Type: text/html; encoding=utf8\n\r\n")
             connectionSocket.send("<html><body><h1>404 ERROR</h1>,</body></html>")
 
         connectionSocket.close()
